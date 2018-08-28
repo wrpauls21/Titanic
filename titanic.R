b4a1d9b86141de241ef98343fad9184f9037eda7
Will Pauls <39473594+wrpauls21@users.noreply.github.com>
2018-08-28 13:14:13 -0500

    added some code

--- titanic.R
+++ titanic.R
@@ -1,3 +1,16 @@
 library(readr)
 titanic_original <- read_csv("C:/Users/wrpauls21/Desktop/Springboard/Titanic/titanic_original.csv")
 View(titanic_original)
+attach(titanic_original)
+
+embarked[is.na(embarked)] <- "S"
+titanic_original$embarked <- embarked
+
+mean_age <- mean(titanic_original$age, na.rm = TRUE)
+age[is.na(age)] <- round(mean(age, na.rm = TRUE))
+titanic_original$age <- age
+
+boat[is.na(boat)] <- "None"
+titanic_original$boat <- boat
+
+titanic_original$has_cabin_number <- NA
